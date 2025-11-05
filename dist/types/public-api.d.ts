e710338de15f16991e0be7d17a99281e912d2608
Marcin Fraś <marcinfras2003@gmail.com>
2025-11-05 18:24:01 +0100

    fix: delete /dist

--- dist/types/public-api.d.ts
+++
@@ -1,6 +0,0 @@
-export { UserType } from '../models/Users.js';
-export { PinStatus, PinType } from '../models/Pins.js';
-export { OAuthError } from './oauth.js';
-export type { MyPin, PinDetails, AdminPin, MapPin, Pagination, listMyPins, listAdminPins, } from '../routes/pins/types.js';
-export type { ApiUser, ApiProfile } from '../routes/auth/types.js';
-//# sourceMappingURL=public-api.d.ts.map
